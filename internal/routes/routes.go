6968f17300827122dcf0f46c6cd2eaacd5426580
Ollie Phillips <ollie@interject.io>
2022-04-26 12:13:51 +0100

    home and list handlers mostly done

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/spoonboy-io/dujour/internal"
@@ -13,18 +15,62 @@ type App struct {
 }
 
 func (a *App) Home(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("Home"))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "text/text")
+
+	res := "Dujour - JSON/CSV Data Server\n"
+	res += "=============================\n\n"
+	res += "Usage\n=====\n"
+	res += "GET / \t\t\t- Text format help page\n"
+	res += "GET /list \t\t- JSON array of all loaded datasources\n"
+	res += "GET /{datasource} \t- JSON representing all elements/rows for requested {datasource} or 404\n"
+	res += "GET /{datasource}/{id} \t- JSON representing element/row matching {id} from requested {datasource} or 404\n"
+
+	_, _ = fmt.Fprint(w, res)
 }
 
 func (a *App) ListDatasources(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("List"))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	// this is the information we will output for list
+	type listDS struct {
+		Endpoint string `json:"endpoint"`
+		Source   string `json:"source"`
+		Elements int    `json:"elements"`
+	}
+
+	list := []listDS{}
+
+	// iterate the datasources
+	for _, v := range a.Datasources {
+		ds := listDS{
+			v.EndpointName,
+			v.FileName,
+			0, // need to add this
+		}
+
+		list = append(list, ds)
+	}
+
+	res, err := json.MarshalIndent(list, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = fmt.Fprint(w, string(res))
 }
 
 func (a *App) DatasourceGetAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	_, _ = w.Write([]byte("GetAll"))
 }
 
 func (a *App) DatasourceGetByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	_, _ = w.Write([]byte("Get By ID"))
 }
 
